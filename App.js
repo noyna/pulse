b1c4e72a2266b457cc1219763d8b9a1ee5a45ee6
NOYNA <penguin_vinci@hotmail.com>
2018-10-21 13:01:24 +0700

    ส่งข้อมูลให้ Client

--- App.js
+++ App.js
@@ -1,1 +1,17 @@
-55555
+const express = require("express")
+const app = express()
+
+app.get("/" ,(req , res)  =>  {
+
+    let co1 = coordinateCreator("16:00", 58)
+    let co2 = coordinateCreator("16:05", 60)
+
+    const result = [co1,co2]
+    res.json(result)
+})
+
+function coordinateCreator(time,bpm) {
+    return {time: time, bpm: bpm}
+}
+
+app.listen(3000)
